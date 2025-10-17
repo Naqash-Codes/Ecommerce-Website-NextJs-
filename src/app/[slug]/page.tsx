4e0787be59d3d9f689d4b457427090fe5e1b6026
Naqash-Codes <naqash.codes@gmail.com>
2025-10-17 18:51:00 +0500

    Single Page Done

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import ProductImages from "../components/ProductImages";
+import CustomizeProducts from "../components/CustomizeProducts";
+import Add from "../components/Add";
 
 const SinglePage = () => {
   return (
-    <div>
-      SinglePage
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/* TEXT */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, iusto
+          error, culpa odio quo porro repellendus voluptatem beatae facere vel
+          maxime, deserunt autem blanditiis.
+        </p>
+        <div className="h-[2px] bg-gray-100 " />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100 " />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100 " />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente sed ad, unde accusantium omnis, veritatis nobis dicta natus, modi nostrum atque distinctio quae ea doloremque reiciendis. Corporis, expedita consequuntur!</p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente sed ad, unde accusantium omnis, veritatis nobis dicta natus, modi nostrum atque distinctio quae ea doloremque reiciendis. Corporis, expedita consequuntur!</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SinglePage
+export default SinglePage;
